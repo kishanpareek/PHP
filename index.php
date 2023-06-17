d88c5ee52d33a607a05df204eead4f93c61498dc
Kishan Pareek <kishan25pareek@gmail.com>
2023-06-17 12:46:15 +0530

    Added Request method

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <title>Document</title>
 </head>
 <body>
-<form action="<?php $_SERVER["PHP_SELF"]?>" method="post">
+<form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post">
   <!-- The form will submit the data to the current PHP script (the same file) when the submit button is clicked -->
 
   username:<br>
@@ -16,6 +16,23 @@
   <input type="submit">
   <!-- Submit button to trigger the form submission -->
 </form>
+
+<?php
+
+
+/*PHP $_REQUEST is a PHP super global variable which is used to collect data after submitting an HTML form.
+
+The example below shows a form with an input field and a submit button. When a user submits the data by clicking on "Submit", the form data is sent to the file specified in the action attribute of the <form> tag. In this example, we point to this file itself for processing form data. If you wish to use another PHP file to process form data, replace that with the filename of your choice. Then, we can use the super global variable $_REQUEST to collect the value of the input field:*/
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+  $username = $_REQUEST['username'];
+  if (empty($username)){
+    echo"Name is empty";
+  } else{
+    echo $username;
+  }
+}
+
+?>
 
 </body>
 </html>
