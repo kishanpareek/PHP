c62d9e09395bdc50080d37bee7095429e180417f
Kishan Pareek <kishan25pareek@gmail.com>
2023-06-17 12:31:56 +0530

    Added comments

--- index.php
+++ index.php
@@ -6,11 +6,17 @@
   <title>Document</title>
 </head>
 <body>
-  <form action="<?php $_SERVER["PHP_SELF"]?>" method="post">
-    username:<br>
-    <input type="text" name="username">
-    <input type="submit" >
-  </form>
+<form action="<?php $_SERVER["PHP_SELF"]?>" method="post">
+  <!-- The form will submit the data to the current PHP script (the same file) when the submit button is clicked -->
+
+  username:<br>
+  <input type="text" name="username">
+  <!-- Text input field where the user can enter their username -->
+
+  <input type="submit">
+  <!-- Submit button to trigger the form submission -->
+</form>
+
 </body>
 </html>
 
